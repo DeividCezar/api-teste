eb0bb4f9a69c00f5d2eecd9e4d10974d93af7ee4
DeividCezar <perspicilum@gmail.com>
2024-09-30 18:30:58 -0300

    Update data.tf

--- eks/data.tf
+++ eks/data.tf
@@ -13,3 +13,23 @@ data "aws_subnet" "subnet" {
   for_each = toset(data.aws_subnets.subnets.ids)
   id       = each.value
 }
+
+data "aws_iam_policy" "cluster_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+data "aws_iam_policy" "resource_controller" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+data "aws_iam_policy" "worker_node_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+data "aws_iam_policy" "cni_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+data "aws_iam_policy" "container_registry_read_only_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
