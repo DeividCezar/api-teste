06a705b5300017d1dc9be1d95992738e731e052a
DeividCezar <perspicilum@gmail.com>
2024-09-30 00:25:43 -0300

    Update data.tf

--- eks/data.tf
+++ eks/data.tf
@@ -1,16 +1,12 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ecr_authorization_token" "token" {}
-
 data "aws_vpc" "vpc" {
   cidr_block = "172.31.0.0/16"
 }
 
 data "aws_subnets" "subnets"{
-  filter {
-    name = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
-  }
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.vpc.id]
+    }
 }
 
 data "aws_subnet" "subnet" {
