320502633cac7072362125cd0221d5bf1e903057
DeividCezar <perspicilum@gmail.com>
2024-09-30 22:37:28 -0300

    Update data.tf

--- eks/data.tf
+++ eks/data.tf
@@ -6,6 +6,27 @@ terraform {
   }
 }
 
+# Definição das políticas IAM necessárias
+data "aws_iam_policy" "cluster_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+data "aws_iam_policy" "resource_controller" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+data "aws_iam_policy" "worker_node_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+data "aws_iam_policy" "cni_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+data "aws_iam_policy" "container_registry_read_only_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 data "aws_vpc" "vpc" {
   cidr_block = "172.31.0.0/16"
 }
@@ -53,6 +74,12 @@ resource "aws_eks_cluster" "eks-cluster" {
     security_group_ids = [aws_security_group.sg.id]  # Referência ao Security Group
   }
 
+  depends_on = [
+    data.aws_iam_policy.worker_node_policy,
+    data.aws_iam_policy.cni_policy,
+    data.aws_iam_policy.container_registry_read_only_policy
+  ]
+  
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
   }
@@ -75,6 +102,12 @@ resource "aws_eks_node_group" "eks-node" {
   update_config {
     max_unavailable = 1
   }
+
+  depends_on = [
+    data.aws_iam_policy.worker_node_policy,
+    data.aws_iam_policy.cni_policy,
+    data.aws_iam_policy.container_registry_read_only_policy
+  ]
 }
 
 # Provider do Kubernetes usando as credenciais do EKS
