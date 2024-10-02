db1f77dfbbb2d4e1b26f9a76b83c25cdae2ace66
DeividCezar <perspicilum@gmail.com>
2024-10-01 21:44:51 -0300

    teste

---
+++ eks/eks-cluster.tf
@@ -0,0 +1,18 @@
+# EKS Cluster
+resource "aws_eks_cluster" "lanchonete-api" {
+  name     = "module-eks-${var.cluster_name}"
+  role_arn = var.node_role_arn
+
+  vpc_config {
+    subnet_ids         = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "sa-east-1e"]
+    security_group_ids = [aws_security_group.sg.id]
+  }
+
+  access_config {
+    authentication_mode = var.accessConfig
+  }
+}
+
+data "aws_eks_cluster_auth" "lanchonete-api_auth" {
+  name = aws_eks_cluster.lanchonete-api.name
+}
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "node_role_arn" {
+  description = "ARN of the IAM Role that will be associated with the Node Group"
+  type        = string
+  sensitive   = true
+  default     = "arn:aws:iam::105971623004:role/LabRole"
+}
+
+variable "kubernetes_namespace" {
+  description = "The Kubernetes namespace where the resources will be provisioned"
+  type        = string
+  default     = "default"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS Cluster"
+  type        = string
+  default     = "lanchonete-api"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  #  default     = "t2.micro"
+  default     = "t3.medium"
+}
+
+variable "datatabase_lanchonete_api_name" {
+  description = "Nome do database do projeto"
+  default     = "rds-lanchonete-api"
+  type        = string
+}
+
+variable "principalArn" {
+  default = "arn:aws:iam::105971623004:role/LabRole"
+}
+
+variable "policyArn" {
+  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
+
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
+}
+
+variable "awsRegion" {
+  default = "us-east-1"
+}
